5928c89d3133b58489259835d0e95d07cd203445
juan fernando <jfermaestre@hotmail.com>
2013-04-26 20:19:00 -0500

    Hello Drew
    I think I understood it.

--- mysite/code/Test.php
+++ mysite/code/Test.php
@@ -15,7 +15,7 @@ class Test_Controller extends ContentController {
 	/**
 	 * An array of actions that can be accessed via a request. Each array element should be an action name, and the
 	 * permissions or conditions required to allow the user to access it.
-	 *
+	 * Hello Drew
 	 * <code>
 	 * array (
 	 *     'action', // anyone can access this action
